0acf18f3f929ff39a6bd2f390ae6fb0e2edae24c
Njoro007 <njorophil@gmail.com>
2025-10-10 15:41:31 +1000

    Add support for new DiscExplode type
    Added the `DiscExplode` class to represent a new disc type in the `ConnectFour` namespace. Updated `Disc.cs` to include mappings for the symbols `'E'` and `'e'` to create `DiscExplode` instances and return the type `"Explode"`.
    
    Removed unused `using` directives from `DiscSpin.cs` and added necessary `using` directives to `DiscExplode.cs` to support the new class.

---
+++ ConnectFour/DiscExplode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectFour
+{
+    public class DiscExplode : Disc
+    {
+        public DiscExplode(char symbol) : base(symbol) { }
+
+
+        public override void ApplyEffect(Disc[,] grid, int row, int col, GameInventory inventory)
+        {
+            // no effect
+        }
+    }
+}
