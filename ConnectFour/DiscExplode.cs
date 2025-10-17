625527f59077d93f18f740462f01cfd82a22d305
Njoro007 <njorophil@gmail.com>
2025-10-17 14:39:27 +1000

    Explode Effect

--- ConnectFour/DiscExplode.cs
+++ ConnectFour/DiscExplode.cs
@@ -13,7 +13,44 @@ namespace ConnectFour
 
         public override void ApplyEffect(Disc[,] grid, int row, int col, GameInventory inventory)
         {
-            // no effect
+            int[] dr = { -1, -1, -1, 0, 0, 1, 1, 1 };
+            int[] dc = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+            grid[row, col] = null; // Remove the exploding disc itself
+
+            for (int i = 0; i < dr.Length; i++)
+            {
+                int r = row + dr[i];
+                int c = col + dc[i];
+
+                if (r >= 0 && r < grid.GetLength(0) && c >= 0 && c < grid.GetLength(1))
+                {
+                    grid[r, c] = null;
+                }
+            }
+
+            // Reapply gravity to affected columns
+            for (int c = col - 1; c <= col + 1; c++)
+            {
+                if (c >= 0 && c < grid.GetLength(1))
+                {
+                    List<Disc> columnDiscs = new List<Disc>();
+                    for (int r = 0; r < grid.GetLength(0); r++)
+                    {
+                        if (grid[r, c] != null)
+                            columnDiscs.Add(grid[r, c]);
+                    }
+
+                    for (int r = 0; r < grid.GetLength(0); r++)
+                        grid[r, c] = r < columnDiscs.Count ? columnDiscs[r] : null;
+                }
+            }
+
+
+
+
+
+
         }
     }
 }
